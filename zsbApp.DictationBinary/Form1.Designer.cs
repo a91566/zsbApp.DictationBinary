d6710d9daf265c1f3e5a85cca890b9c003a4d864
zhengShaobao <a91566@163.com>
2018-07-01 17:11:27 +0800

    Initial commit

---
+++ zsbApp.DictationBinary/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace zsbApp.DictationBinary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnBegin = new System.Windows.Forms.ToolStripButton();
+            this.btnDone = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslClock = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbBinary = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnBegin,
+            this.btnDone});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(871, 42);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnBegin
+            // 
+            this.btnBegin.Image = global::zsbApp.DictationBinary.Properties.Resources.Start_48px_1186321_easyicon_net;
+            this.btnBegin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBegin.Name = "btnBegin";
+            this.btnBegin.Size = new System.Drawing.Size(60, 39);
+            this.btnBegin.Text = "开始";
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Image = global::zsbApp.DictationBinary.Properties.Resources.yes_49_548387096774px_1138946_easyicon_net;
+            this.btnDone.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(60, 39);
+            this.btnDone.Text = "提交";
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslClock});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 373);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(871, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslClock
+            // 
+            this.tslClock.Name = "tslClock";
+            this.tslClock.Size = new System.Drawing.Size(0, 17);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 42);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown2);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txbBinary);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            this.splitContainer1.Panel1MinSize = 386;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txbLog);
+            this.splitContainer1.Size = new System.Drawing.Size(871, 331);
+            this.splitContainer1.SplitterDistance = 386;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "-";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(209, 37);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(169, 26);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "二进制";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "十进制";
+            // 
+            // txbBinary
+            // 
+            this.txbBinary.Location = new System.Drawing.Point(81, 69);
+            this.txbBinary.Name = "txbBinary";
+            this.txbBinary.Size = new System.Drawing.Size(297, 26);
+            this.txbBinary.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(81, 37);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(106, 26);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // txbLog
+            // 
+            this.txbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txbLog.Location = new System.Drawing.Point(0, 0);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ReadOnly = true;
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbLog.Size = new System.Drawing.Size(481, 331);
+            this.txbLog.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(871, 395);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "二进制默写";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbBinary;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ToolStripStatusLabel tslClock;
+        private System.Windows.Forms.ToolStripButton btnBegin;
+        private System.Windows.Forms.ToolStripButton btnDone;
+    }
+}
+
---
+++ zsbApp.DictationBinary/Form1.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * 2018年7月1日 11:03:01 郑少宝
+ */
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace zsbApp.DictationBinary
+{
+    public partial class Form1 : Form
+    {
+        private Timer tClock;
+
+        private Dictionary<int, string> dictUserDictation;
+
+        private System.Diagnostics.Stopwatch watch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Shown += (s, e) => {
+                this.setDictation(false);
+                this.tClock = new Timer();
+                this.tClock.Enabled = true;
+                this.tClock.Interval = 1000;
+                this.tClock.Tick += (s1, e1) => this.tslClock.Text = System.DateTime.Now.ToString("HH:mm:ss");
+                this.txbBinary.KeyPress += (s1, e1) => {
+                    if (((int)e1.KeyChar < 48 || (int)e1.KeyChar > 49) && (int)e1.KeyChar != 8)
+                        e1.Handled = true;
+                };
+                this.txbBinary.KeyDown += (s1, e1) => {
+                    if (e1.KeyData == Keys.Enter)
+                        this.add();
+                };
+            };
+        }
+
+        private void setDictation(bool start)
+        {
+            this.tslClock.Visible = start;
+            this.txbBinary.Enabled = start;
+            this.btnDone.Enabled = start;
+            this.btnBegin.Enabled = !start;
+            this.numericUpDown1.Enabled = !start;
+            this.numericUpDown2.Enabled = !start;
+        }
+
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            this.setDictation(true);
+            this.dictUserDictation = new Dictionary<int, string>();
+            this.watch = new System.Diagnostics.Stopwatch();
+            this.watch.Start();
+        }
+
+        private const string RIGHT = "√";
+        private const string WRONG = "×";
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            this.setDictation(false);
+            this.watch.Stop();
+            long totaltime = this.watch.ElapsedMilliseconds;
+            this.txbLog.Clear();
+            if (this.dictUserDictation.Count == 0)
+                return;
+            int okCount = 0;
+            foreach (var item in this.dictUserDictation)
+            {
+                var actual = Convert.ToString(item.Key, 2);
+                if (actual == item.Value)
+                {
+                    this.txbLog.AppendText($"{item.Key}>{item.Value} {RIGHT}{System.Environment.NewLine}");
+                    okCount++;
+                }
+                else
+                {
+                    this.txbLog.AppendText($"{item.Key}>{item.Value} {WRONG} {item.Key}的二进制为：{actual}{System.Environment.NewLine}");
+                }
+            }
+            var baifen = (okCount * 100 / this.dictUserDictation.Count).ToString("f2");
+            this.txbLog.AppendText($"总耗时：{totaltime / 1000}秒,正确数：{okCount},正确率:{baifen}%{System.Environment.NewLine}");
+        }
+
+        private void add()
+        {
+            if (string.IsNullOrEmpty(this.txbBinary.Text.Trim()))
+                return;
+            this.dictUserDictation.Add((int)this.numericUpDown1.Value, this.txbBinary.Text.Trim());
+            this.txbLog.AppendText($"{this.numericUpDown1.Value}>{this.txbBinary.Text.Trim()}{System.Environment.NewLine}");
+            this.txbBinary.Clear();
+            this.numericUpDown1.Value++;
+            if (this.numericUpDown1.Value >= this.numericUpDown2.Value)
+                this.btnDone.PerformClick();
+        }
+    }
+}
